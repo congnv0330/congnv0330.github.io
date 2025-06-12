16459079e745a2ab456499ceec42984e8686ff6a
congnv3030 <congnv3030@gmail.com>
2025-06-12 08:23:44 +0700

    fix: tailwindcss setup not correct

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite'
 import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig({
-  plugins: [webfontDownload()],
+  plugins: [tailwindcss(), webfontDownload()],
 });
